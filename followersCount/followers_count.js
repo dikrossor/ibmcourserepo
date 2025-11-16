6834202ed130d9fe5c3d8f92b110e6e96b5d256c
dikrossor <dikrossor@ukr.net>
2025-11-16 13:52:54 -0500

    add practice task to followers count lab

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -17,3 +17,9 @@ function checkCountValue() {
         alert("Your Instagram post gained 20 followers! Keep it up!");
     }
 }
+
+function reset() {
+    count = 0
+    document.getElementById('countDisplay').innerHTML = count;
+    alert("The counter has been reset");
+}
